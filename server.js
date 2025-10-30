942ce3b1013dbe64b6f5d29d8c0b6ac7479d5389
nicolaschianchiitis <snicola.schianchi@itis.pr.it>
2025-10-30 03:16:31 +0100

    Finito server: problemini col video in Safari, ma sappiamo la soluzione

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const path = require('path')
 const hostname = "127.0.0.1"
 const port = 3000
 
+// Messaggi di errore
+const err404 = '<center><h1 style="border: 2px solid red">Errore 404: risorsa non trovata.</h1></center>'
+const err500 = '<center><h1 style="border: 2px solid red">Errore 500: errore interno del server.</h1></center>'
+
 // Oggetto risolutore del MIME type, data l'estensione file
 const mimeTypes = {
     '.html': 'text/html',
@@ -29,37 +33,64 @@ const filePaths = {
     '/video_pista_atletica': './public/video/video_pista_atletica.mp4'
 }
 
-async function requestHandler(req, res) {
-    console.log(`>>> Richiesta in entrata: ${req.url}`)
-    
-    /* let reqPath = filePaths[req.url]
-    let extension = path.extname((reqPath == undefined ? '' : reqPath))
-    let encoding = (extension == '.jpeg' || extension == '.png') ? '' : 'utf-8'
+/*
+    Attenzione!
+    Questo metodo "universale" funziona perfettamente
+    con Chrome (e suppongo anche browser basati su
+    Chromium) e Firefox (anche qua, suppongo anche
+    coi svariati fork). In generale, funziona
+    senza troppe seccature.
+    Safari, come al solito, richiede un trattamento
+    più delicato, ovvero quello di gestire manualmente
+    la risposta del server, con codice 206 (partial
+    content) e bisogna gestire l'header Range della
+    richiesta per inviare i byte richiesti.
+*/
 
-    // Versione con async/await
-    try {
-        const data = await fs.readFile((reqPath == undefined ? '' : reqPath), {'encoding': encoding})
-        res.writeHead(200, {'Content-Type': mimeTypes[extension]})
-        res.write(data)
-        res.end()
-    } catch (err) {
-        res.writeHead(404, { 'Content-Type': mimeTypes['.html'] })
-        res.write(`<center><h1 style="border: 4px solid red;">Errore 404: risorsa non trovata.</h1></center>`)
-        res.end()
-    } */
+function requestHandler(req, res) {
+    console.log(`~~~> Richiesta in entrata: ${req.url}`)
 
-    /* // Versione con funzioni asincrone e callback
-    fs.readFile((reqPath == undefined ? '' : reqPath), {'encoding': encoding}, function (err, data) {
-        if (err) {
-            res.writeHead(404, { 'Content-Type': mimeTypes['.html'] })
-            res.write(`<center><h1 style="border: 4px solid red;">Errore 404: risorsa non trovata.</h1></center>`)
+    if (filePaths.hasOwnProperty(req.url)) {
+        const filePath = filePaths[req.url]
+        const contentType = mimeTypes[path.extname(filePath)] || 'application/octet-stream'
+        // Creazione stream di lettura per il file
+        const fileStream = fs.createReadStream(filePath)
+
+        // Preparazione header 200 OK (Success)
+        res.writeHead(200, {'Content-Type': contentType})
+
+        // Gestione eventuali errori
+        fileStream.on('error', function(err) {
+            console.error(`!! Errore ${err.code}. Messaggio: ${err.message}`)
+            if (err.code === 'ENOENT') {
+                res.writeHead(404, {'Content-Type': 'text/html'})
+                res.end(err404)
+            } else {
+                res.writeHead(500, {'Content-Type': 'text/html'})
+                res.end(err500)
+            }
+        })
+
+        // Scriviamo i dati direttamente nella risposta e inviamo una volta finito
+        fileStream.pipe(res)
+
+        // Versione alternativa
+        /*
+            Carichiamo i dati in memoria a piccoli blocchetti (chunk)
+            e scriviamo man mano nella risposta
+        */
+        /* fileStream.on('data', function(chunk) {
+            res.write(chunk)
+        }) */
+
+        // Inviamo la risposta al client una volta finito
+        /* fileStream.on('end', function() {
             res.end()
-        } else {
-            res.writeHead(200, {'Content-Type': mimeTypes[extension]})
-            res.write(data)
-            res.end()
-        }
-    }) */
+        }) */
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/html'})
+        res.end(err404)
+    }
 }
 
 const server = http.createServer(requestHandler)
